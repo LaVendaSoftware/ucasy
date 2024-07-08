11490fb2b9a4bffb6e030b1622dd2e4da825dd2c
Walmir <owalmirneto@gmail.com>
2024-07-08 07:25:54 -0300

    feat: create validator using ActiveModel::Model

--- lib/ucasy/base.rb
+++ lib/ucasy/base.rb
@@ -1,5 +1,6 @@
 require_relative "context"
 require_relative "failure"
+require_relative "validators/validate"
 require_relative "validators/required_attributes"
 
 module Ucasy
@@ -8,6 +9,13 @@ module Ucasy
       def call(context = {})
         new(context).perform
       end
+
+      def validate(validator_class, validatable_attributes = nil)
+        @validator_class = validator_class
+        @validatable_attributes = validatable_attributes
+      end
+
+      attr_reader :validator_class, :validatable_attributes
 
       def required_attributes(*attributes)
         @required_attributes = attributes
@@ -25,7 +33,9 @@ module Ucasy
     def perform
       return if failure?
 
+      validate!
       validate_required_attributes!
+
       try(:before) if success?
       call if success?
       try(:after) if success?
@@ -38,6 +48,20 @@ module Ucasy
     private
 
     attr_reader :context
+
+    def validate!
+      return if self.class.validator_class.blank?
+
+      validator = Validators::Validate.call(*validate_params)
+
+      return if validator.valid?
+
+      context.fail!(status: :unprocessable_entity, errors: validator.errors)
+    end
+
+    def validate_params
+      [self.class.validator_class, try(self.class.validatable_attributes) || context]
+    end
 
     def validate_required_attributes!
       Validators::RequiredAttributes.call(context, self.class._required_attributes, self.class)
