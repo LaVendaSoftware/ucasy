bdd94f2f7d921b44589decc46763299b7d5bc96c
Walmir <owalmirneto@gmail.com>
2024-07-08 22:01:56 -0300

    chore: pass current class to required attributes

--- lib/ucasy/validators/required_attributes.rb
+++ lib/ucasy/validators/required_attributes.rb
@@ -1,15 +1,16 @@
 module Ucasy::Validators
   class RequiredAttributes < Ucasy::Callable
-    def initialize(context, required_attributes)
+    def initialize(context, required_attributes, klass)
       @context = context
       @required_attributes = required_attributes
+      @klass = klass
     end
 
     def call
       @required_attributes.each do |attribute|
         next if @context.respond_to?(attribute)
 
-        raise ArgumentError, "You must set '#{attribute}' variable in '#{self.class}'"
+        raise ArgumentError, "You must set '#{attribute}' variable in '#{@klass}'"
       end
     end
   end
