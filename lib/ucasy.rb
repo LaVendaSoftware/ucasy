86202beeea42f5ce4533a7023f52329099f4b8b9
Walmir <owalmirneto@gmail.com>
2025-05-25 11:32:04 -0300

    [Feat] Create generator to copy code to application (#12)
    * refactor: load file using autoload method
    
    * feat: create generator to copy code to application
    
    * test: remove versions in Gemfile
    
    * test: update rails and ruby version in rubocop config file
    
    * chore: update version to 0.3.2

--- lib/ucasy.rb
+++ lib/ucasy.rb
@@ -1,10 +1,19 @@
 require_relative "ucasy/version"
-require_relative "ucasy/callable"
-require_relative "ucasy/base"
-require_relative "ucasy/flow"
 
 module Ucasy
   Ucasy::DEFAULT_GENERATOR_FOLDER_NAME = "use_cases"
+
+  autoload :Callable, "ucasy/callable"
+  autoload :Context, "ucasy/context"
+  autoload :Failure, "ucasy/failure"
+
+  module Validators
+    autoload :Validate, "ucasy/validators/validate"
+    autoload :RequiredAttributes, "ucasy/validators/required_attributes"
+  end
+
+  autoload :Base, "ucasy/base"
+  autoload :Flow, "ucasy/flow"
 
   class Error < StandardError
   end
